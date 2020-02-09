435e34a8dcc6b64d2ce512d7000df856b4e62506
Turkhan Badalov <turkhan.badalov@ut.ee>
2020-02-09 03:19:21 +0200

    Use bootstrap grid system, centralize the page vertically

--- src/index.js
+++ src/index.js
@@ -5,8 +5,22 @@ import style from "./style.css"
 
 const Index = () => {
   return(
-    <div className="container">
-      <DropArea/>
+    <div class="container-fluid">
+      <div class="row">
+        <div className="col text-center">
+          <h1 className="header">URLIFY</h1>
+        </div>
+      </div>
+      <div class="row margin-bottom">
+        <div className="col text-center">
+          <h3 className="header">Extract and share links from your presentation</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div className="col text-center">
+          <DropArea/>
+        </div>
+      </div>
     </div>
   );
 };
