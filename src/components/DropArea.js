2cff4b6de250ef2fe154eef99bf47cbf991e14b7
Turkhan Badalov <turkhan.badalov@ut.ee>
2020-02-17 01:34:42 +0200

    Remove uploader once server responded

--- src/components/DropArea.js
+++ src/components/DropArea.js
@@ -5,22 +5,39 @@ import customStyle from '../droparea.css';
 
 const areaId = 'dropArea';
 
-Dropzone.options[areaId] = {
-  paramName: "file", // The name that will be used to transfer the file
-  maxFilesize: 2, // MB
-  accept: function(file, done) {
-    if (file.name == "justinbieber.jpg") {
-      done("Naha, you don't.");
-    }
-    else { done(); }
-  },
-  url: "/dropalwik"
-};
+Dropzone.autoDiscover = false;
 
-const DropArea = () => {
-  return(
-    <div className="dropzone" id={areaId}></div>
-  );
+class DropArea extends Component {
+
+  constructor(props) {
+    super(props)
+    this.configureDropzone({
+      onSuccess: props.onSuccess.bind(this),
+    })
+  }
+
+  configureDropzone(callbacks) {
+    Dropzone.options[areaId] = {
+      paramName: "file", // The name that will be used to transfer the file
+      maxFilesize: 5, // MB
+      init: function() {
+        this.on('success', function(response) { callbacks.onSuccess({code: 200, presentationId: 117}) });
+      },
+      url: "/presentations",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      dropzone: new Dropzone(`div#${areaId}`, Dropzone.options[areaId]),
+    });
+  }
+
+  render() {
+    return(
+      <div className="dropzone" id={areaId}></div>
+    );
+  }
 };
 
 export default DropArea;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import DropArea from './components/DropArea';
 import style from "./style.css"
 
-const Index = () => {
-  return(
-    <div class="container-fluid">
-      <div class="row">
-        <div className="col text-center">
-          <h1 className="header">URLIFY</h1>
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onPresentationUploaded: this.handleUploadedPresentation.bind(this)
+    };
+  }
+
+  handleUploadedPresentation(response) {
+    const changeStateToShowPresentation = (presentation) => {
+      this.setState({
+        showPresentation: true,
+        presentationId: presentation.id,
+      });
+    };
+
+    if (response.code != 200) {
+      return;
+    }
+
+    changeStateToShowPresentation({ id: response.presentationId });
+  }
+
+  render() {
+    function replacedComponent(state) {
+      if (state.showPresentation) {
+        return <Presentation presentationId={state.presentationId} />;
+      }
+
+      if (state.isProcessing) {
+        return <Loading />
+      }
+
+      return <DropArea onSuccess={state.onPresentationUploaded} />;
+    }
+
+    return(
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col text-center">
+            <h1 className="header">URLIFY</h1>
+          </div>
+        </div>
+        <div className="row margin-bottom">
+          <div className="col text-center">
+            <h3 className="header">Extract and share links from your presentation</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center">
+            { replacedComponent(this.state) }
+          </div>
         </div>
       </div>
-      <div class="row margin-bottom">
-        <div className="col text-center">
-          <h3 className="header">Extract and share links from your presentation</h3>
-        </div>
-      </div>
-      <div class="row">
-        <div className="col text-center">
-          <DropArea/>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 ReactDOM.render(<Index />, document.getElementById("app"));
